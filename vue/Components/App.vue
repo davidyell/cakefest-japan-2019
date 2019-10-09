7df2e3ab7a2e37240121b2b340e774a566622b16
David Yell <dyell@comparisontech.com>
2019-10-09 16:34:09 +0100

    Implemented a filter

--- vue/Components/App.vue
+++ vue/Components/App.vue
@@ -1,12 +1,44 @@
 <template>
   <div id="task-list">
     <h1>Task list</h1>
-    <task
-      v-for="task in tasks"
-      :key="task.id"
-      :task="task"
-      @toggleComplete="toggleComplete"
-    />
+
+    <div id="filter">
+      <form>
+        <div class="row">
+          <div class="columns medium-3">
+            <label
+              for="filter-complete"
+            >Filter tasks</label>
+            <select
+              id="filter-complete"
+              v-model="filter.completed"
+            >
+              <option :value="null">
+                All tasks
+              </option>
+              <option :value="false">
+                Pending
+              </option>
+              <option :value="true">
+                Completed
+              </option>
+            </select>
+          </div>
+          <div class="columns medium-9">&nbsp;</div>
+        </div>
+      </form>
+    </div>
+
+    <div class="row">
+      <div class="columns medium-12">
+        <task
+          v-for="task in filteredTasks"
+          :key="task.id"
+          :task="task"
+          @toggleComplete="toggleComplete"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,8 +53,22 @@ export default {
   },
   data () {
     return {
-      tasks: null
+      tasks: null,
+      filter: {
+        completed: null
+      }
     };
+  },
+  computed: {
+    filteredTasks () {
+      if (this.filter.completed === true) {
+        return this.tasks.filter(task => task.is_complete === true);
+      } else if (this.filter.completed === false) {
+        return this.tasks.filter(task => task.is_complete === false);
+      }
+
+      return this.tasks;
+    }
   },
   mounted () {
     const component = this;
