f95428b486e34c80fcc29e0a04cee302641e986b
David Yell <dyell@comparisontech.com>
2019-10-10 14:54:16 +0100

    Added Vuex, for state management

--- vue/app.js
+++ vue/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './Components/App.vue';
+import { store } from './Store/Store.js';
 
 // Don't try and load Vue.js application where it's not needed
 if (document.getElementById('app')) {
-  new Vue({
+  const app = new Vue({
     el: '#app',
+    store: store,
     render: h => h(App)
   });
 }
