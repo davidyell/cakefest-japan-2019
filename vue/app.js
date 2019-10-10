f7ce0194e50c1a09d02e2903569acc649f5e9cd3
David Yell <dyell@comparisontech.com>
2019-10-10 15:09:58 +0100

    No unused variables

--- vue/app.js
+++ vue/app.js
@@ -4,7 +4,7 @@ import { store } from './Store/Store.js';
 
 // Don't try and load Vue.js application where it's not needed
 if (document.getElementById('app')) {
-  const app = new Vue({
+  new Vue({
     el: '#app',
     store: store,
     render: h => h(App)
